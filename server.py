209e3d777b6bfe9e95b0b2e651a17e1627cc17f9
potzpf <potzpf@scramble.io>
2019-06-26 10:57:55 +0200

    ...

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
-from flask import Flask, redirect, url_for, request, abort, jsonify
+from flask import Flask, render_template, request, abort, jsonify
 from app.database import DbHandler
 from app.routes import *
 
 app = Flask(__name__)
-
 db = DbHandler("data/store.json")
 
 @app.route("/")
 def hello():
-    return redirect(url_for('static', filename="contacts.html"))
+    return render_template("contacts.html")
 
 # Returns a list of all contacts with basic information
 @app.route("/contacts", methods=["GET"])
 def getContacts():
-    list = db.getContactList()
+    try:
+        list = db.getContactList()
+    except Exception as e:
+        app.logger.warn(f"Unknown Exception: {e}")
+        abort(500)
+
     return jsonify(list)
 
 
@@ -33,7 +37,14 @@ def postContact():
 # Returns full information on a given contact
 @app.route("/contact/<int:id>", methods=["GET"])
 def getContact(id):
-    contact = db.getContact(id)
+    try:
+        contact = db.getContact(id)
+    except ValueError:
+        abort(400)
+    except Exception as e:
+        app.logger.warn(f"Unknown Exception: {e}")
+        abort(500)
+
     return jsonify(contact)
 
 # Updates contact information
